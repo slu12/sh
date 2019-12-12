f5daec8b95841d8be73de8a84c20e0ad1e854d8f
chenwei <chenwei@shuzilm.cn>
2019-12-13 07:36:09 +0800

    api

---
+++ dragon_ship_api/apis/res.go
@@ -0,0 +1,15 @@
+/**
+@Name : chenwei
+@Time : 2019/12/13 7:33 上午
+**/
+package apis
+
+type SingleResult struct {
+	Status int
+	Result interface{}
+}
+type ListResult struct {
+	Result []interface{}
+	Status int
+	Total  int
+}

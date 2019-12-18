9ba3c9f8d9de43cf8fee1551e30864901bd1d2d2
youaretheappleinmyeyes <965567396@qq.com>
2019-12-18 12:57:34 +0800

    aron

--- scrn/src/scrn/comp/pager4/index.vue
+++ scrn/src/scrn/comp/pager4/index.vue
@@ -49,6 +49,27 @@
         v.domeEH.h = v.AF.getDom_H('carouselBox4')
         v.domeEH.w = v.AF.getDom_W('carouselBox4')
       })
+    },
+    methods:{
+      buildMap() {
+        // 百度地图API功能
+        var map = new BMap.Map("allmap");
+        map.centerAndZoom(new BMap.Point(116.404, 39.915), 11);
+
+        var top_left_control = new BMap.ScaleControl({anchor: BMAP_ANCHOR_TOP_LEFT});// 左上角，添加比例尺
+        var top_left_navigation = new BMap.NavigationControl();  //左上角，添加默认缩放平移控件
+        var top_right_navigation = new BMap.NavigationControl({
+          anchor: BMAP_ANCHOR_TOP_RIGHT,
+          type: BMAP_NAVIGATION_CONTROL_SMALL
+        }); //右上角，仅包含平移和缩放按钮
+        /*缩放控件type有四种类型:
+        BMAP_NAVIGATION_CONTROL_SMALL：仅包含平移和缩放按钮；BMAP_NAVIGATION_CONTROL_PAN:仅包含平移按钮；BMAP_NAVIGATION_CONTROL_ZOOM：仅包含缩放按钮*/
+
+        //添加控件和比例尺
+        map.addControl(top_left_control);
+        map.addControl(top_left_navigation);
+        map.addControl(top_right_navigation);
+      }
     }
   }
 </script>

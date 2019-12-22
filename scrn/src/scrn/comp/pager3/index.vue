8a62490641a2572f9c3ba4135b47824812d67438
youaretheappleinmyeyes <965567396@qq.com>
2019-12-22 20:58:40 +0800

    aron

--- scrn/src/scrn/comp/pager3/index.vue
+++ scrn/src/scrn/comp/pager3/index.vue
@@ -37,20 +37,60 @@
         // 百度地图API功能
         let pot = new BMap.Point(114.298273, 30.560577)
         this.map = new BMap.Map("carouselBox3", {minZoom: 4, maxZoom: 20});    // 创建Map实例
-        this.map.centerAndZoom(pot, 12);  // 初始化地图,设置中心点坐标和地图级别
-        // this.map.enableScrollWheelZoom(true);    //开启鼠标滚轮缩放
+        this.map.centerAndZoom(pot, 9);  // 初始化地图,设置中心点坐标和地图级别
+        this.map.enableScrollWheelZoom(true);    //开启鼠标滚轮缩放
         // this.map.enableDragging();
-        this.map.addEventListener('click', function (val) {
-          console.log(val);
+        this.map.addEventListener('click', function (e) {
+          // alert(e.point.lng + "," + e.point.lat);
+          console.log(e.point.lng + "," + e.point.lat);
         })
         this.map.addEventListener('rightclick', function (val) {
           v.sysEvent()
         })
-        this.buildCircle(pot)
+        // this.buildCircle(pot)
+        this.buildPolygon()
       },
       buildCircle(pot){
         var circle = new BMap.Circle(pot,5000,{strokeColor:"blue", strokeWeight:2, strokeOpacity:0.5}); //创建圆
         this.map.addOverlay(circle);
+      },
+      buildPolygon(){
+        let pots = [
+          new BMap.Point(111.983861,30.293206),
+          new BMap.Point(111.999177,30.281785),
+          new BMap.Point(112.026571,30.275691),
+          new BMap.Point(112.080656,30.28593),
+          new BMap.Point(112.217021,30.310205),
+          new BMap.Point(112.262423,30.211215),
+          new BMap.Point(112.193138,30.152732),
+          new BMap.Point(112.193138,30.152732),
+          new BMap.Point(112.250629,30.060731),
+          new BMap.Point(112.355058,29.967668),
+          new BMap.Point(112.382654,29.694965),
+          new BMap.Point(113.086351,29.417485),
+          new BMap.Point(114.144196,30.051733),
+          new BMap.Point(114.622525,30.502843),
+          new BMap.Point(116.135703,29.658815),
+          new BMap.Point(118.476759,31.244741),
+          new BMap.Point(119.42422,32.153133),
+          new BMap.Point(119.405823,32.266537),
+          new BMap.Point(118.81251,32.244551),
+          new BMap.Point(116.140302,29.911585),
+          new BMap.Point(114.466148,30.781238),
+          new BMap.Point(113.120846,29.536212),
+          new BMap.Point(112.994365,29.8274),
+          new BMap.Point(112.442446,29.922103),
+          new BMap.Point(112.380355,30.087739),
+          new BMap.Point(112.243525,30.093738),
+          new BMap.Point(112.311365,30.28753),
+          new BMap.Point(112.235476,30.327439),
+          new BMap.Point(112.197532,30.324446),
+          new BMap.Point(111.992844,30.300939),
+          new BMap.Point(111.983861,30.293206)
+        ]
+        var polygon = new BMap.Polygon(pots, {strokeColor:"blue", strokeWeight:2, strokeOpacity:0.5});  //创建多边形
+        this.map.addOverlay(polygon);
+        this.map.setViewport(pots)
       },
       sysEvent(){
         this.$emit('sysEvent',3)
--- scrn/src/scrn/comp/pager4/index.vue
+++ scrn/src/scrn/comp/pager4/index.vue
@@ -1,22 +1,15 @@
 <template>
-  <div class="box_col pager1Sty"@contextmenu.prevent="sysEvent">
+  <div class="box_col pager1Sty" @contextmenu.prevent="sysEvent">
     <div class="box_row pagerTop">
       <div class="pager4Tit" @click="sysEvent">摄像头视频</div>
     </div>
     <div id="carouselBox4" class="box_col_100">
       <div v-if="domeEH.w>0&&domeEH.h>0"
+           style="position: relative"
            :style="{width:domeEH.w+'px',height:domeEH.h+'px'}">
-        <!--<Carousel v-model="CarouselConfig.val"-->
-                  <!--:loop="CarouselConfig.loop"-->
-                  <!--:autoplay="CarouselConfig.autoplay"-->
-                  <!--:autoplay-speed="CarouselConfig.autoplaySpeed"-->
-        <!--&gt;-->
-          <!--<CarouselItem v-for="(it,index) in CarouselConfig.fileUrl" :key="index">-->
-            <!--<div class="" :style="{width:domeEH.w+'px',height:domeEH.h+'px'}">-->
-              <img :src="itUrl" style="width: 100%;height: 100%" alt="">
-            <!--</div>-->
-          <!--</CarouselItem>-->
-        <!--</Carousel>-->
+        <img v-if="itUrl!==''" :src="itUrl" style="width: 100%;height: 100%" alt="">
+        <Icon v-else type="ios-images-outline" color="#ffffff" size="100"
+              style="position: absolute;left: 50%;top: 50%;transform: translate(-50%,-50%)"/>
       </div>
     </div>
     <div class="pager4footer box_row rowBetween">
@@ -39,42 +32,49 @@
     name: "index",
     data() {
       return {
-        CarouselConfig:{
-          val:3,
-          loop:true,
-          autoplay:false,
-          autoplaySpeed:5000,
-          fileUrl:[login1,login2,login1,login2]
+        CarouselConfig: {
+          val: 3,
+          loop: true,
+          autoplay: false,
+          autoplaySpeed: 5000,
+          fileUrl: [login1, login2, login1, login2]
         },
-        domeEH:{
-          w:0,
-          h:0
+        domeEH: {
+          w: 0,
+          h: 0
         },
-        itUrl:login1
+        itUrl: ""
       }
     },
     mounted() {
       var v = this
-      this.$nextTick(()=>{
+      this.$nextTick(() => {
         v.domeEH.h = v.AF.getDom_H('carouselBox4')
         v.domeEH.w = v.AF.getDom_W('carouselBox4')
       })
+      this.setIntervalEvent()
     },
-    methods:{
-      sysEvent(){
-        this.$emit('sysEvent',4)
+    methods: {
+      sysEvent() {
+        this.$emit('sysEvent', 4)
       },
-      photo(){
-        this.$http.post('/pub/zp',{sbh:'34286',chn:'0'}).then(res=>{
-          if(res.code == 200){
-            this.$Message.success('抓拍成功');
+      setIntervalEvent(){
+        this.photo()
+        setTimeout(()=>{
+          this.setIntervalEvent()
+        },10500)
+      },
+      photo() {
+        this.$http.post('/pub/zp', {sbh: '34286', chn: '0'}).then(res => {
+          if (res.code == 200) {
+            // this.$Message.success('抓拍成功');
             this.itUrl = res.message
-          }else {
-            this.$Message.warning(res.message);
+          } else {
+            // this.$Message.warning(res.message);
           }
         })
       },
-      videoEvent(){
+      videoEvent() {
         this.$emit('videoEvent')
       }
     }
@@ -98,22 +98,22 @@
       }
     }
 
-    .pager4footer{
+    .pager4footer {
       padding: 0 14px;
-      height:40px;
-      background:rgba(0,0,0,1);
-      border-top:1px solid rgba(94,104,125,1);
+      height: 40px;
+      background: rgba(0, 0, 0, 1);
+      border-top: 1px solid rgba(94, 104, 125, 1);
       height: 40px;
       line-height: 40px;
 
-      font-size:14px;
-      font-family:Microsoft YaHei;
-      font-weight:400;
-      color:rgba(255,255,255,1);
+      font-size: 14px;
+      font-family: Microsoft YaHei;
+      font-weight: 400;
+      color: rgba(255, 255, 255, 1);
     }
 
-    .iconItem{
-      cursor:pointer;
+    .iconItem {
+      cursor: pointer;
     }
   }
 

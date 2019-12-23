76b31edc852f8ef3ab1e5e0a5cdca11ca16cdab0
youaretheappleinmyeyes <965567396@qq.com>
2019-12-24 00:22:06 +0800

    aron

--- ShenHai/src/viewDevelop/Shipmanagement/ship-mess/superSearch/comp/searchGroup/index.vue
+++ ShenHai/src/viewDevelop/Shipmanagement/ship-mess/superSearch/comp/searchGroup/index.vue
@@ -2,15 +2,12 @@
   <div>
     <Select
       v-model="searchGroupVal"
-      size="small"
-      multiple
-      filterable
-      remote
-      :remote-method="remoteMethod"
-      :loading="searchGroupLoading"
-      style="width: 360px"
-    >
-      <Option v-for="(option, index) in optionsList" :value="option.value" :key="index">{{option.label}}</Option>
+      clearable
+      @on-change="getSearchGroupVal"
+      @on-clear = "clearEvent"
+      style="width: 360px">
+      <Option v-for="(option, index) in optionsList" :value="option.id"
+              :key="index">{{option.text}}</Option>
     </Select>
   </div>
 </template>
@@ -21,27 +18,37 @@
     data(){
       return {
         searchGroupVal:"",
-        searchGroupLoading:false,
         optionsList:[]
       }
     },
+    created(){
+      this.getOptionsList()
+    },
     methods:{
-      remoteMethod (query) {
-        if (query !== '') {
-          this.searchGroupLoading = true;
-          setTimeout(() => {
-            this.searchGroupLoading = false;
-            const list = this.list.map(item => {
-              return {
-                value: item,
-                label: item
-              };
-            });
-            this.searchGroupLoading = list.filter(item => item.label.toLowerCase().indexOf(query.toLowerCase()) > -1);
-          }, 200);
-        } else {
-          this.options2 = [];
-        }
+      getOptionsList(){
+        this.$http.get('/api/cbcd/pager').then(res=>{
+          res.page.list.forEach((it,index)=>{
+            it.jsonP = JSON.parse(it.json)
+            it.text = ""
+            for(let a in it.jsonP){
+              it.text =it.text + it.jsonP[a].value + '、'
+            }
+            if(index == res.page.list.length - 1){
+              this.optionsList = res.page.list
+            }
+          })
+        }).catch(err=>{})
+      },
+      getSearchGroupVal(val){
+        // console.log(val);
+        this.optionsList.forEach((it,index)=>{
+          if(it.id == val){
+            this.$emit('getSearchGroup',it.json)
+          }
+        })
+      },
+      clearEvent(){
+        this.$emit('clearEvent')
       }
     }
   }

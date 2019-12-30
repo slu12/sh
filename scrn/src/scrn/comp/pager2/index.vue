ec79f7fe5ddb8ddecc7d1b426074786cf4058cbe
youaretheappleinmyeyes <965567396@qq.com>
2019-12-31 00:10:13 +0800

    aron

--- scrn/src/scrn/comp/pager2/index.vue
+++ scrn/src/scrn/comp/pager2/index.vue
@@ -19,26 +19,29 @@
 </template>
 
 <script>
+  import cbImg from '../file/cb.png'
+
   export default {
     name: "index",
     data() {
       return {
-        domeEH:{
-          w:0,
-          h:0
+        cbImg,
+        domeEH: {
+          w: 0,
+          h: 0
         }
       }
     },
     mounted() {
       var v = this
-      this.$nextTick(()=>{
-        setTimeout(()=>{
+      this.$nextTick(() => {
+        setTimeout(() => {
           v.buildMap()
-        },100)
+        }, 100)
       })
     },
-    methods:{
-      buildMap(){
+    methods: {
+      buildMap() {
         var v = this
         // 百度地图API功能
         this.map = new BMap.Map("carouselBox2", {minZoom: 4, maxZoom: 20});    // 创建Map实例
@@ -49,65 +52,73 @@
           console.log(val);
         })
         this.map.addEventListener('rightclick', function (val) {
-         v.sysEvent()
+          v.sysEvent()
         })
-
+        this.get_CB_Code()
         this.get_GJ_Code()
       },
-      get_GJ_Code(){
+      get_CB_Code() {//获取船舶定位点
+
+        this.$http.post('/pub/currentGps', {mmsi: 413839203}).then(res => {
+          console.log(res);
+          if (res.code == 200) {
+            this.addMarkCode(res.result.jd, res.result.wd, res.result.fxj)
+          }
+        }).catch(err => {})
+      },
+      addMarkCode(bdjd, bdwd, fxj) {
+        console.log(bdjd);
+        console.log(bdwd);
+        var myIcon = new BMap.Icon(cbImg, new BMap.Size(60, 15), {imageSize: new BMap.Size(60, 15)}, {anchor: new BMap.Size(30, 17.5)});
+        let point = new BMap.Point(bdjd, bdwd)
+
+        var marker = new BMap.Marker(point, {icon: myIcon});  // 创建标注
+        marker.setRotation(parseFloat(fxj) - 90)
+        this.map.addOverlay(marker);               // 将标注添加到地图中
+        this.map.setCenter(new BMap.Point(bdjd, bdwd))
+      },
+      get_GJ_Code() {//获取轨迹点
         var v = this
         let a = new Date()
-        this.$http.post('/pub/newXc',{
-          mmsi:"413839203",
-          start:this.moment(a.getTime()-48*60*60*1000).format('YYYY-MM-DD HH:mm:ss'),
-          end:this.moment().format('YYYY-MM-DD HH:mm:ss')
-        }).then(res=>{
-          if(res.code == 200){
-            let pois = [
-              // new BMap.Point(116.350658,39.938285),
-              // new BMap.Point(116.386446,39.939281),
-              // new BMap.Point(116.389034,39.913828),
-              // new BMap.Point(116.442501,39.914603),
-              // new BMap.Point(111.0715,21.479398333333332),
-              // new BMap.Point(111.07177,21.479750000000003)
-            ];//new BMap.Point(116.350658,39.938285)
-
-
-            res.result.forEach((it,index)=>{
-              if(index == 0){
-                // v.map.setCenter(new BMap.Point(it.longitude,it.latitude))
+        this.$http.post('/pub/newXc', {
+          mmsi: "413839203",
+          start: this.moment(a.getTime() - 24 * 60 * 60 * 1000).format('YYYY-MM-DD HH:mm:ss'),
+          end: this.moment().format('YYYY-MM-DD HH:mm:ss')
+        }).then(res => {
+          if (res.code == 200) {
+            let pois = [];
+            res.result.forEach((it, index) => {
+              if (index == 0) {
+                // v.map.setCenter(new BMap.Point(it.longitude, it.latitude))
                 // v.map.setZoom(22)
               }
-              pois.push(new BMap.Point(it.longitude,it.latitude))
-              if(index == res.result.length-1){
+              pois.push(new BMap.Point(it.longitude, it.latitude))
+              if (index == res.result.length - 1) {
                 var sy = new BMap.Symbol(BMap_Symbol_SHAPE_BACKWARD_OPEN_ARROW, {
                   scale: 0.6,//图标缩放大小
-                  strokeColor:'#fff',//设置矢量图标的线填充颜色
+                  strokeColor: '#fff',//设置矢量图标的线填充颜色
                   strokeWeight: '2',//设置线宽
                 });
                 var icons = new BMap.IconSequence(sy, '10', '30');
-                var polyline =new BMap.Polyline(pois, {
+                var polyline = new BMap.Polyline(pois, {
                   enableEditing: false,//是否启用线编辑，默认为false
                   enableClicking: true,//是否响应点击事件，默认为true
-                  icons:[icons],
-                  strokeWeight:'8',//折线的宽度，以像素为单位
+                  icons: [icons],
+                  strokeWeight: '8',//折线的宽度，以像素为单位
                   strokeOpacity: 0.8,//折线的透明度，取值范围0 - 1
-                  strokeColor:"#18a45b" //折线颜色
+                  strokeColor: "#18a45b" //折线颜色
                 });
 
                 v.map.addOverlay(polyline);          //增加折线
                 v.map.setViewport(pois)
               }
             })
-
-
           }
-        }).catch(err=>{
-
+        }).catch(err => {
         })
       },
-      sysEvent(){
-        this.$emit('sysEvent',2)
+      sysEvent() {
+        this.$emit('sysEvent', 2)
       }
     }
   }
@@ -118,7 +129,7 @@
     background-color: #5E687D;
     .pagerTop {
       height: 40px;
-      .textTit{
+      .textTit {
         text-align: center;
         font-size: 14px;
         font-family: Microsoft YaHei;
@@ -131,14 +142,14 @@
         background: rgba(54, 62, 79, 1);
         .textTit;
       }
-      .cbName{
+      .cbName {
         .textTit;
       }
-      .cbType{
+      .cbType {
         .textTit;
       }
     }
-    #carouselBox{
+    #carouselBox {
       background-color: #FFFFFF;
     }
   }

9439d3cd7d02bc9548fd1075542f7d642932aa4d
youaretheappleinmyeyes <965567396@qq.com>
2020-01-16 23:15:44 +0800

    aron

--- ShenHai/src/view/mapDemo/index.vue
+++ ShenHai/src/view/mapDemo/index.vue
@@ -3,15 +3,33 @@
     <div id="viewDiv" class="box_col_100"></div>
   </div>
 </template>
-
 <script>
+  import * as esriLoader from 'esri-loader'
+  // import esriLoader from '@/libs/esri'
 
+  // require([
+  //   "esri/Map",
+  //   "esri/views/MapView"
+  // ], function (Map, MapView) {
+  //
+  //   var map = new Map({
+  //     basemap: "topo-vector"
+  //   });
+  //
+  //   var view = new MapView({
+  //     container: "viewDiv",
+  //     map: map,
+  //     center: [-118.71511, 34.09042],
+  //     zoom: 11
+  //   });
+  //
+  // });
 
   export default {
     name: "index",
     mounted(){
       this.$nextTick(()=>{
-        // this.buildMap()
+        this.buildMap2()
       })
     },
     methods:{
@@ -173,126 +191,38 @@
         });
       },
       buildMap2(){
-        define([
-          "dojo/has",
-          "esri/config",
-          "esri/WebMap",
-          "esri/layers/CSVLayer",
-          "esri/views/MapView",
-          "esri/widgets/Legend",
-          "esri/widgets/Expand"
-        ], function (has, esriConfig,WebMap, CSVLayer, MapView,Legend, Expand) {
+        console.log('===');
+        console.log(esriLoader);
+        const options = {
+          url: 'https://js.arcgis.com/4.14/'
+        }
+        esriLoader.loadModules([
+          'esri/map',
+          'dojo/domReady!'
+        ], options).then(([Map]) => {
+          console.log('===',Map);
+          // var map = new Map({
+          //     basemap: "topo-vector"
+          //   });
+          //
+          //   var view = new MapView({
+          //     container: "viewDiv",
+          //     map: map,
+          //     center: [-118.71511, 34.09042],
+          //     zoom: 11
+          //   });
 
-          if (!has("dojo-built")) {
-            esriConfig.workers.loaderConfig = {
-              paths: {
-                "esri": "../arcgis-js-api",
-                "dstore": "../dojo-dstore"
-              }
-            };
-          }
-
-          var url =
-            "https://arcgis.github.io/arcgis-samples-javascript/sample-data/hurricanes.csv";
-
-          var csvLayer = new CSVLayer({
-            title: "Hurricanes",
-            url: url,
-            copyright: "NOAA",
-            popupTemplate: {
-              title: "{Name}",
-              content: [{
-                type: "text",
-                text: "Category {Category} storm with that occurred at {ISO_time}."
-              }, {
-                type: "fields",
-                fieldInfos: [{
-                  fieldName: "wmo_pres",
-                  label: "Pressure"
-                }, {
-                  fieldName: "wmo_wind",
-                  label: "Wind Speed (mph)"
-                }]
-              }],
-              fieldInfos: [{
-                fieldName: "ISO_time",
-                format: {
-                  dateFormat: "short-date-short-time"
-                }
-              }]
-            },
-            renderer: {
-              type: "unique-value",
-              field: "Category",
-              uniqueValueInfos: createUniqueValueInfos()
-            }
-          });
-
-          var map = new WebMap({
-            // contains a basemap with a South Pole Stereographic projection
-            // the CSVLayer coordinates will re-project client-side
-            // with the Projection Engine to match the view's Spatial Reference
-            basemap: {
-              portalItem: {
-                id: "3113eacc129942b4abde490a51aeb33f"
-              }
-            },
-            layers: [csvLayer]
-          });
-
-          var view = new MapView({
-            container: "viewDiv",
-            map: map,
-            highlightOptions: {
-              color: "#ff642e",
-              haloOpacity: 1,
-              fillOpacity: 0.25
-            },
-            popup: {
-              dockEnabled: true,
-              dockOptions: {
-                breakpoint: false
-              }
-            }
-          });
-
-          var legendExpand = new Expand({
-            view: view,
-            content: new Legend({
-              view: view,
-              style: "card"
-            })
-          });
-          view.ui.add(legendExpand, "top-left");
-
-          function createUniqueValueInfos() {
-            var fireflyImages = [
-              "cat1.png",
-              "cat2.png",
-              "cat3.png",
-              "cat4.png",
-              "cat5.png"
-            ];
-
-            var baseUrl =
-              "https://arcgis.github.io/arcgis-samples-javascript/sample-data/";
-
-            return fireflyImages.map(function (url, i) {
-              return {
-                value: i + 1, // Category number
-                symbol: {
-                  type: "picture-marker",
-                  url: baseUrl + url
-                }
-              }
-            });
-          }
-        });
+          let map = new Map('viewDiv  ', {
+            basemap: 'topo',
+            center: [113.3209952545166, 23.090055306224895],
+            zoom: 15
+          })
+        }).catch (err => {})
       }
     }
   }
 </script>
 
 <style lang="less">
-/*@import "https://js.arcgis.com/4.14/esri/css/main.css";*/
+@import "https://js.arcgis.com/4.14/esri/css/main.css";
 </style>
